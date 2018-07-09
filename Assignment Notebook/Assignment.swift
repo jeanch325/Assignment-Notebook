e91bcfd3b5c62b8023297f1954f65e3ad8ec07d4
Jean Cho <jeancho@jeans-macbook-air.local>
2018-07-09 11:44:15 -0500

    Set up StoryBoard

--- Assignment Notebook/Assignment.swift
+++ Assignment Notebook/Assignment.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class Assignment: Codable {
     var courseName : String
+    var assignmentTitle : String
     var dueDate : String
     var description : String
     var image : Data
     
-    init(courseName: String, dueDate: String, description: String, image: Data) {
+    init(courseName: String, assignmentTitle: String, dueDate: String, description: String, image: Data) {
         self.courseName = courseName
+        self.assignmentTitle = assignmentTitle
         self.dueDate = dueDate
         self.description = description
         self.image = image
